8e8d2b6777e8bd04a84441b7e359d4adae27b828
dungviettran89 <dungviettran89@gmail.com>
2019-05-13 22:13:13 +0800

    #3: Testing proxy

--- npm/go-len-tools/src/main.js
+++ npm/go-len-tools/src/main.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 
 (async () => {
+    let port = 28789;
     let process = require('process');
-    let env = {...process.env, GOPROXY: "https://athens.cuatoi.us"};
+    let env = {...process.env, GOPROXY: `http://localhost:${port}`};
     let stdio = 'inherit';
     let goArgs = process.argv.splice(2);
+    let express = require('express')();
+    express.use('/', require('express-http-proxy')('https://athens.cuatoi.us'));
+    let server = express.listen(port, () => console.log(`GoLen proxy listening on port ${port}!`));
     let go = require('cross-spawn')('go', goArgs, {env, stdio: stdio});
     await new Promise((resolve => go.on('exit', resolve).on('error', resolve)));
+    server.close();
 })();
